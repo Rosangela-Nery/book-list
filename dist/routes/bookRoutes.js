c80c4b82260e4fab23489837b52a145a89a4a15b
Rosângela Nery <101907764+Rosangela-Nery@users.noreply.github.com>
2022-11-12 21:32:39 -0300

    Delete dist directory

--- dist/routes/bookRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { validationBook } from '../middlewares/validation.js';
-import { bookPost, bookGet } from '../controllers/bookControllers.js';
-var router = express.Router();
-router.post("/book", validationBook, bookPost);
-router.get("/book", bookGet);
-export default router;
